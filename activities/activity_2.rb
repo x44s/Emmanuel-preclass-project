048151b0656e812fdb9ea183382854aea9a64208
Johnpjrs <johnemmanuelpajares@gmail.com>
2023-05-15 16:39:51 +0800

    Activity 2: Functions of returns of Full name from givin hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,10 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def full_name(names)
+  return "#{names[:first_name]} #{names[:middle_name]} #{names[:last_name]}"
+end
+
+names={first_name: 'John Emmanuel', middle_name: 'Manago', last_name: 'Pajares'}
+p full_name(names)
